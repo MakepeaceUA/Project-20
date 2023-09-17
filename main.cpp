b5f0fca0892f0dda77fb90f255c27d3db336bbbb
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-09-17 16:24:21 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+
+//Задание 1
+//#define Even(X) (X % 2 == 0 ? true : false)
+
+//int main() 
+//{
+//   int num = 4;
+
+//   cout << Even(num);
+//}
+
+
+
+//Задание 2
+//int Sum(int N) 
+//{
+//  int sum = 0;
+//  for (int i = 1;i <= N;i++)
+//    sum += i;
+
+//  return sum;
+//}
+//#define Macr(N) (Sum(N))
+
+//int main() 
+//{
+//   int num = 10;
+//   cout << Macr(num);
+//}
+
+
+
+//Задание 3
+//int Func(int Num, int De) 
+//{
+//  int res = Num;
+//  for (int i = 1;i < De;i++)
+//   res *= Num;
+
+//  return res;
+//}
+//#define Macr(X,Y) (Func((X),(Y)))
+
+//int main() 
+//{
+//   int num = 2;
+//   int degree = 2;
+//   cout << Macr(num,degree);
+//}
+
+
+
+//Задание 4
+//int Func(int N) 
+//{
+// if (N == 0) 
+//   return 0;
+// else if (N >= 0)
+//   cout << "*";
+//   return Func(N - 1);
+//}
+
+//int main() 
+//{
+//int num = 5;
+//Func(num);
+//}
+
+
+
+//Задание 5
+//void Func(int N) 
+//{
+//  if (N > 0) 
+//  {
+//     Func(N - 1); 
+//     cout << N << " ";  
+//    }
+//}
+
+//int main() 
+//{
+// int num = 10;
+// Func(num);
+//}
+
+
+
+
+
+
